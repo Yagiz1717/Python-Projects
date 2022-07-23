3b759feea9e8ee32ca8b1118cbe8d08917f07ce2
Yağız <59937583+Yagiz1717@users.noreply.github.com>
2022-07-23 17:34:12 +0300

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-Urunler = [
-    {"id":0,"Urunadi":"klavye","Urunfiyat":200},
-    {"id":1,"Urunadi":"mouse","Urunfiyat":150},
-    {"id":2,"Urunadi":"mousepad","Urunfiyat":35},
-    {"id":3,"Urunadi":"kulaklık","Urunfiyat":350},
-    {"id":4,"Urunadi":"mikrofon","Urunfiyat":400},
-    
-]
